e638054aba93141a3f36e93b6a9fcf480cc235eb
rob <52644668+robyoung75@users.noreply.github.com>
2020-10-06 11:48:56 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+//Styles
+import "./App.css";
+
+//Components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Feed from "./components/Feed/Feed";
+import Widgets from "./components/Widgets/Widgets";
+import Login from "./components/Login/Login";
+import { useStateValue } from "./StateProvider";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  return (
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
